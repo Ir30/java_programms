f145b3c1e26d7da712d298e737c72186dae4249b
irshad <irshadmuhammedch8@gmail.com>
2022-09-23 11:14:22 +0530

    pattern printing

---
+++ pattern.java
@@ -0,0 +1,24 @@
+public class pattern {
+    public static void main(String args[]){
+
+                for(int j=1;j<=5;j++){
+
+                    for(int i=1;i<=5;i++){
+                        System.out.print("*  ");
+                    }
+                    System.out.println();
+                }
+
+                for(int j=1;j<=5;j++){
+
+                    for(int i=1;i<=j;i++){
+
+                        System.out.print(i+" ");
+                    }
+                    System.out.println();
+                }
+            
+            
+     }
+    
+}
