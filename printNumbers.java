7dabcb1a1e48a7e69accb8d92a279c77bdc97ed3
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-14 19:25:41 +0530

    coment added in printNumbers

--- printNumbers.java
+++ printNumbers.java
@@ -2,46 +2,44 @@ import java.util.Scanner;
 
 public class printNumbers {
     static void printing_numbers(int num){
-        String newNum =String.valueOf(num);
-        // System.out.println(newNum);
 
-        String res="";
+        String newNum =String.valueOf(num);//converting intiger to string 
+        String[] newArray=newNum.split("");//splitting each numbers of converted string and storing in a string array
+
+        //creating arrays of numbers in words
         String[] onedigit = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};  
         String[] twodigits = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};  
         String[] multipleoftens = new String[] {"",  "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}; 
-        // System.out.println(newNum.length());
-        // System.out.println("----------"+newNum.split("")[0]);
 
-        
+//cheking the length of numbers to identify the digit(one or two or three)
         if(newNum.length()==1){
-            System.out.println(onedigit[num]);
-            
+            System.out.println(onedigit[num]);//if it is one then take element direct from onedigit array    
         }
-        else if(newNum.length()==2){
-            // System.out.println("yes");
-            // System.out.println((newNum.split("")[0]));
-            if((newNum.split("")[0]).equals("1")){
-                // System.out.println("yes");
-                System.out.println(twodigits[Integer.parseInt(newNum.split("")[1])]);    
+        else if(newNum.length()==2){//if it is two digit 
+
+            if(newArray[0].equals("1")){//if it is two digit and the first element is one - because itis a special case it become ten,eleven,... rather than twenty one,thirty four....
+                System.out.println(twodigits[Integer.parseInt(newArray[1])]);
             }
             else{
-                System.out.println(multipleoftens[Integer.parseInt(newNum.split("")[0])]+" "+onedigit[Integer.parseInt(newNum.split("")[1])]);
+                System.out.println(multipleoftens[Integer.parseInt(newArray[0])]+" "+onedigit[Integer.parseInt(newArray[1])]);
             }
         }
-        else{
-            if((newNum.split("")[1]).equals("1")){
-                System.out.println(onedigit[Integer.parseInt(newNum.split("")[0])]+" Hundred "+twodigits[Integer.parseInt(newNum.split("")[2])]);
+        else{//if it is two digit
+            if(newArray[1].equals("1")){//if it is two digit and second element is one 
+                System.out.println(onedigit[Integer.parseInt(newArray[0])]+" Hundred "+twodigits[Integer.parseInt(newArray[2])]);
             }
             else{
-                System.out.println(onedigit[Integer.parseInt(newNum.split("")[0])]+" Hundred "+multipleoftens[Integer.parseInt(newNum.split("")[1])]+" "+onedigit[Integer.parseInt(newNum.split("")[2])]);
+                System.out.println(onedigit[Integer.parseInt(newArray[0])]+" Hundred "+multipleoftens[Integer.parseInt(newArray[1])]+" "+onedigit[Integer.parseInt(newArray[2])]);
             }
         }
         
     }
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num = sc.nextInt();
+
         printNumbers.printing_numbers(num);
 }
 }
